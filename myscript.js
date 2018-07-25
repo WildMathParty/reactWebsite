e91198f962347beb9964f7be7e1814205ca8786e
Henry Thomas <14306@wc.school.nz>
2018-07-25 14:15:31 +1200

    need jquery

---
+++ myscript.js
@@ -0,0 +1,20 @@
+//let clueType = document.getElementById("clueType").value;
+
+//if(clueType == "clueString"){
+//	document.getElementById("p2").innerHTML = "blue";
+//}
+
+/*clueType.onchange = function(){
+	let selIndex = clueType.selectedIndex;
+	let selValue = clueType.options(selIndex).innerHTML;
+
+	switch(selValue){
+		case "clueJigsaw":
+			document.getElementById("p2").style.color = "blue";
+			break;
+		deafult:
+			return;
+	};
+};*/
+
+document.getElementById("p1").innerHTML = "New text!";
