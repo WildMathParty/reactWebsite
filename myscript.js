af3d2302ac3adf2b0ef3e42417d5f6acfd927f5d
Henry Thomas <14306@wc.school.nz>
2018-07-27 10:16:59 +1200

    select jquery work

--- myscript.js
+++ myscript.js
@@ -1,7 +1,7 @@
 //let clueType = document.getElementById("clueType").value;
 
 //if(clueType == "clueString"){
-//	document.getElementById("p2").innerHTML = "blue";
+//	document.getElementById("p1").innerHTML = "blue";
 //}
 
 /*clueType.onchange = function(){
@@ -10,11 +10,53 @@
 
 	switch(selValue){
 		case "clueJigsaw":
-			document.getElementById("p2").style.color = "blue";
+			document.getElementById("p1").style.color = "blue";
 			break;
 		deafult:
 			return;
 	};
 };*/
 
-document.getElementById("p1").innerHTML = "New text!";
+function stringForms(){
+	let pText = "<p>String selected</p>";
+	$(".formsDiv").append(pText);
+};
+
+function pickerForms(){
+	
+};
+
+function jigsawForms(){
+	
+};
+
+function multiForms(){
+	
+};
+
+$(document).ready(function(){
+
+let clueType = document.getElementById("clueType");
+
+clueType.onchange = function(){
+	let selValue = clueType.value;
+
+	switch(selValue){
+		case "clueString":
+			document.getElementById("p1").style.color = "black";
+			break;
+		case "cluePicker":
+			stringForms();
+			break;
+		case "clueJigsaw":
+			document.getElementById("p1").style.color = "purple";
+			break;
+		case "clueMulti":
+			document.getElementById("p1").style.color = "blue";
+			break;	
+		deafult:
+			return;
+	};
+};
+
+});
