bd45854cd7fcd08ea86beea78d303b08e88ab338
Henry Thomas <14306@wc.school.nz>
2018-07-30 15:14:24 +1200

    clue type select changes forms

--- myscript.js
+++ myscript.js
@@ -18,20 +18,85 @@
 };*/
 
 function stringForms(){
-	let pText = "<p>String selected</p>";
-	$(".formsDiv").append(pText);
+	$(".formEntry").remove();
+	$(".formsDiv").append("<div class='formEntry'></div>");
+
+	let clueForm = `
+		<form>
+		Question Text: <input type="text" name="QuestionText"><br>
+		Answer Text: <input type="text" name="AnswerText"><br>
+		<input type="submit" value="update">
+		</form>
+	`;
+	$(".formEntry").append(clueForm);
 };
 
 function pickerForms(){
-	
+	$(".formEntry").remove();
+	$(".formsDiv").append("<div class='formEntry'></div>");
+
+	let clueForm = `
+		<form>
+		Question Text: <input type="text" name="QuestionText"><br>
+		First Column: <input type="text" name="AnswerText"><br>
+		<input type="button" value="Add Column" onclick="addColumn()"><br>
+		<input type="submit" value="update">
+		</form>
+	`;
+	$(".formEntry").append(clueForm);
 };
 
 function jigsawForms(){
-	
+	$(".formEntry").remove();
+	$(".formsDiv").append("<div class='formEntry'></div>");
+
+	let clueForm = `
+		<form>
+		Upload Picture: <input type="text" name="QuestionText"><br>
+		Pieces Across: <input type="text" name="AnswerText"><br>
+		Pieces Down: <input type="text" name="AnswerText"><br>
+		<input type="submit" value="update">
+		</form>
+	`;
+	$(".formEntry").append(clueForm);
 };
 
 function multiForms(){
-	
+	$(".formEntry").remove();
+	$(".formsDiv").append("<div class='formEntry'></div>");
+
+	let clueForm = `
+		<form>
+		Question Type: <select id="multiType"><br>
+			<option value="multi">Multiple Choice</option><br>
+			<option value="combi">Combinatorial</option><br>
+		</select><br>
+		Question Text: <input type="text" name="QuestionText"><br>
+
+		<div class="answerInputs"><br>		
+		Correct Answer: <input type="text" name="AnswerText"><br>
+		</div><br>
+		<input type="button" value="Add Answer" onclick="addAnswer(1)">
+		<input type="button" value="Remove Answer" onclick="addAnswer(0)"><br>
+
+		<div class="choiceInputs"><br>		
+			Possible Choice: <input type="text" name="ChoiceText"><br>
+		</div><br>
+		<input type="button" value="Add Choice" onclick="addChoice(1)">
+		<input type="button" value="Remove Choice" onclick="addChoice(0)"><br>
+
+		<input type="submit" value="update">
+		</form>
+	`;
+	$(".formEntry").append(clueForm);
+};
+
+function addChoice(add){
+	$(".choiceInputs").append("<p>Test</p>");
+};
+
+function addAnswer(add){
+	$(".answerInputs").append("<p>Test</p>");
 };
 
 $(document).ready(function(){
@@ -43,16 +108,16 @@ clueType.onchange = function(){
 
 	switch(selValue){
 		case "clueString":
-			document.getElementById("p1").style.color = "black";
-			break;
-		case "cluePicker":
 			stringForms();
 			break;
+		case "cluePicker":
+			pickerForms();
+			break;
 		case "clueJigsaw":
-			document.getElementById("p1").style.color = "purple";
+			jigsawForms();
 			break;
 		case "clueMulti":
-			document.getElementById("p1").style.color = "blue";
+			multiForms();
 			break;	
 		deafult:
 			return;
