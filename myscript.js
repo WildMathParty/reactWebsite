dabc91295896cde936bd0b9b59cbbe3743a9a396
Henry Thomas <14306@wc.school.nz>
2018-08-01 11:10:12 +1200

    multi all work

--- myscript.js
+++ myscript.js
@@ -1,4 +1,5 @@
 let multiButtonID = 0;
+let columnID = 0;
 
 function stringForms(){
 	$(".formEntry").remove();
@@ -21,7 +22,9 @@ function pickerForms(){
 	let clueForm = `
 		<form>
 		Question Text: <input type="text" name="QuestionText"><br>
-		First Column: <input type="text" name="AnswerText"><br>
+		<div class="pickerColumns">
+		Picker Column: <input type="text" name="AnswerText"><br>
+		</div>
 		<input type="button" value="Add Column" onclick="addColumn()"><br>
 		<input type="submit" value="update">
 		</form>
@@ -35,9 +38,11 @@ function jigsawForms(){
 
 	let clueForm = `
 		<form>
-		Upload Picture: <input type="text" name="QuestionText"><br>
-		Pieces Across: <input type="text" name="AnswerText"><br>
-		Pieces Down: <input type="text" name="AnswerText"><br>
+		Upload Picture: <form enctype="multipart/form-data" action="/upload/image" method="post">
+    						<input id="image-file" type="file" />
+						</form><br>
+		Pieces Across: <input type="number" min="1" max="6" name="piecesAcross"><br>
+		Pieces Down: <input type="number" min="1" max="6" name="piecesDown"><br>
 		<input type="submit" value="update">
 		</form>
 	`;
@@ -59,12 +64,12 @@ function multiForms(){
 		<div class="answerInputs"><br>		
 		Correct Answer: <input type="text" name="AnswerText"><br>
 		</div><br>
-		<input type="button" value="Add Answer" onclick="addMultiButton('answer')"><br>
 
 		<div class="choiceInputs"><br>		
 			Possible Choice: <input type="text" name="ChoiceText"><br>
+		<div class="extraChoices"></div>
+		<input type="button" value="Add Choice" onclick="addMultiButton('Choices')">
 		</div><br>
-		<input type="button" value="Add Choice" onclick="addMultiButton('choice')"><br>
 
 		<input type="submit" value="update">
 		</form>
@@ -74,19 +79,27 @@ function multiForms(){
 	let multiType = document.getElementById("multiType");
 	multiType.onchange = function(){
 		let selType = multiType.value;
-		
+		switch(selType){
+			case "multi":
+				$(".extraAnswers").remove()
+				$(".answerAdd").remove();
+				break;
+			case "combi":
+				$(".answerInputs").append('<div class="extraAnswers"></div>');
+				$(".answerInputs").append(`<input class="answerAdd" type="button" value="Add Answer" onclick="addMultiButton('Answers')">`);
+				break;
+			default:
+				return;
+		}
 	}
 };
 
 function addMultiButton(add){
 	multiButtonID++;
-	/*$("." + add + "Inputs").append(`
-		<p class="multiButton"`+multiButtonID+`>Test</p>
-		<input class="multiButton"`+multiButtonID+` type="button" value="Remove", onclick="removeMultiButton(`+multiButtonID+`">`);
-	*/
 	let inputTitle
-	add==="answer" ? inputTitle="Correct Answer: " : inputTitle="Possible Choice: ";
-	$("." + add + "Inputs").append(`<div class='multibutton`+multiButtonID+`'</br>
+
+	add==="Answers" ? inputTitle="Correct Answer: " : inputTitle="Possible Choice: ";
+	$(".extra" + add).append(`<div class='multibutton`+multiButtonID+`'</br>
 		`+inputTitle+`<input type="text" name="AnswerText">
 		<input type='button' value='Remove' onclick='removeMultiButton(`+multiButtonID+`)'></br>
 		</div>`);
@@ -94,6 +107,18 @@ function addMultiButton(add){
 
 function removeMultiButton(ID){
 	$(".multibutton"+ID).remove();
+}
+
+function addColumn(ID){
+	columnID++;
+	$("pickerColumns").append(`<div class="column`+columnID+`" </br>
+		Picker Column: <input type="text" name="pickerColumn">
+		<input type="button" value="Remove" onclick="removeColumn(`+columnID+`)"></br>
+		</div>`);
+}
+
+function removeColumn(ID){
+	$(".column" +ID).remove();
 }
 
 $(document).ready(function(){
